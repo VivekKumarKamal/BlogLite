f3749a2e50ed6395b9ab8ba29002287a3e9caf7c
VivekKumarKamal <vivekkumar565950@gmail.com>
2022-12-12 10:57:16 +0530

    initial commit

---
+++ shitt.py
@@ -0,0 +1,13 @@
+def max3bad (x, y, z):
+    maximum = 0
+    if x >= y:
+        if x >= z:
+            maximum = x
+            
+    elif y >= Z:
+        maximum = y
+    else:
+        maximum = z
+    return (maximum)
+
+print(max3bad(3, 2, 5))
